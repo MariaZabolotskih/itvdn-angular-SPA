65b635c954a1e46202eb2a2406231a05e5c60f6b
Maria Zabolotskikh <maria.zabolotskikh@euroml.ru>
2022-06-30 23:15:06 +0300

    added config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SpaConfigService, SpaConfigSettings } from 'src/spa/services/spa-config.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'itvdn-SPA-project';
+export class AppComponent implements OnInit {
+  
+  constructor(private spaConfigService: SpaConfigService) {}
+
+  ngOnInit(): void {
+    const config: SpaConfigSettings = {
+      socialIcons: [
+        { imageFile: 'assets/imgs/vk.png', alt: 'ВКонтакте', url: 'https://vk.com' },
+        { imageFile: 'assets/imgs/whatsapp.png', alt: 'ВКонтакте', url: 'https://whatsapp.com' },
+      ],
+      showUserControls: true
+    };
+    this.spaConfigService.configure(config);
+  }
+
 }
