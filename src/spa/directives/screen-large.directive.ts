b6a331caaa18afbbb9bc0117dd2a7352119030bf
Maria Zabolotskikh <maria.zabolotskikh@euroml.ru>
2022-07-02 09:08:56 +0300

    added directives

---
+++ src/spa/directives/screen-large.directive.ts
@@ -0,0 +1,37 @@
+import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
+import { ScreenService } from '../services/screen.service';
+
+@Directive({
+  selector: '[screenLarge]'
+})
+export class ScreenLargeDirective implements OnInit {
+  @Input() set screenLarge(condition: boolean) {
+    condition = this.screenService.screenWidth >= this.screenService.largePixels;
+    if (condition && !this.hasView) {
+      this.hasView = true;
+      this.viewContainer.createEmbeddedView(this.template);
+    } else if (!condition && this.hasView) {
+      this.hasView = false;
+      this.viewContainer.clear();
+    }
+  }
+  private hasView = false;
+
+  constructor(
+    private template: TemplateRef<Object>, 
+    private screenService: ScreenService, 
+    private viewContainer: ViewContainerRef
+  ) { 
+    screenService.resize$.subscribe(() => {
+      this.onResize();
+    })
+  }
+
+  ngOnInit() {
+    this.onResize();
+  }
+
+  onResize() {
+    this.screenLarge = false;
+  }
+}
