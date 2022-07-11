eb4af85332f89723ab0dc9ed073557595c308723
Maria Zabolotskikh <maria.zabolotskikh@euroml.ru>
2022-07-11 23:48:10 +0300

    added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SpaModule } from '../spa/spa.module';
 import { HomeComponent } from './home/home.component';
-import { GroupsComponent } from './groups/groups.component';
 import { SettingsComponent } from './settings/settings.component';
+import { GroupMaintenanceComponent } from './groups-maintenance/groups-maintenance.component';
+import { GroupsListComponent } from './groups-list/groups-list.component';
+import { GroupDetailComponent } from './group-detail/group-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    GroupsComponent,
-    SettingsComponent
+    SettingsComponent,
+    GroupMaintenanceComponent,
+    GroupsListComponent,
+    GroupDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/groups-maintenance/groups-maintenance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-groups-maintenance',
+  templateUrl: './groups-maintenance.component.html',
+  styleUrls: ['./groups-maintenance.component.scss']
+})
+export class GroupMaintenanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
